ae859b4a32b01a0100b29186e2e5dcbf8229d0ad
Lidar-Mark <lidarmark@gmail.com>
2024-08-25 12:56:39 +0300

    added mongodb init

--- tests/tasks_sanity_test.py
+++ tests/tasks_sanity_test.py
@@ -2,27 +2,22 @@ import pytest
 from pymongo import MongoClient
 from src.main import app
 
-
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
-
-# fix code duplication
 @pytest.fixture(scope="module")
 def mongo_client():
     # Use the same connection string as in your app
     client = MongoClient('mongodb://admin:admin@127.0.0.1:27017/')
     db = client['tasks']
     yield db
-    # Cleanup after tests
-    client.drop_database('tasks')
-
-
+    # No cleanup after tests, data will persist
+    # You can clear specific collections if desired, e.g.:
+    # db['important-tasks'].delete_many({})
 
 def test_create_task(client, mongo_client):
-    # Test data
     task_data = {
         "name": "Write tests",
         "people_involved": ["John", "Jane"],
@@ -40,7 +35,6 @@ def test_create_task(client, mongo_client):
     assert task_in_db["name"] == "Write tests"
     assert task_in_db["people_involved"] == ["John", "Jane"]
     assert task_in_db["description"] == "Write tests for the task API."
-
 
 def test_read_task(client, mongo_client):
     # Insert a task directly into the database for testing
